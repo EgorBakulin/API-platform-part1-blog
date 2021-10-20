03daa398b17f3f0ce3ffe9e54ea829665d731eff
ebakulin <egor.bakulin@noveogroup.com>
2021-10-20 23:21:29 +0700

    DTO AND DATATRANSFORMER IN REQUEST: added Dto and DataTransformer for post

---
+++ src/DataTransformer/BlogPostInputPostDataTransformer.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataTransformer;
+
+use ApiPlatform\Core\DataTransformer\DataTransformerInterface;
+use App\Dto\BlogPostInputPost;
+use App\Entity\BlogPost;
+use DateTime;
+
+class BlogPostInputPostDataTransformer implements DataTransformerInterface
+{
+
+    public function transform($object, string $to, array $context = []): BlogPost
+    {
+        $blogPost = new BlogPost();
+
+        $blogPost
+            ->setCreatedAt(new DateTime())
+            ->setTitle($object->title)
+            ->setContent($object->content)
+            ->setOnReview(true);
+    
+        return $blogPost;
+    }
+
+    public function supportsTransformation($data, string $to, array $context = []): bool
+    {
+        return (
+            ($context['operation_type'] === 'collection') &&
+            ($context['collection_operation_name'] === 'post') &&
+            ($context['input']['class'] === BlogPostInputPost::class) && 
+            ($to === BlogPost::class)
+        );
+    }
+}
---
+++ src/Dto/BlogPostInputPost.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Dto;
+
+class BlogPostInputPost
+{
+    public string $title;
+
+    public string $content;
+}
